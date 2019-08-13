a467257242fcb4dee6d706cc3c990f662799cb61
许道斌 <xudb01@mingyuanyun.com>
2019-08-13 15:54:21 +0800

    update: 优化一些细节

--- src/view/home/article/index.js
+++ src/view/home/article/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import '@/common/less/article.less'
-import { Carousel } from 'antd';
+import { getArticleDate } from '@/common/utils/utils.js'
+import { Carousel, Skeleton, BackTop } from 'antd';
 import { getTags, getArticle } from '@/js/api.js'
 class Article extends Component {
     constructor(props) {
@@ -45,40 +46,6 @@ class Article extends Component {
             }
             return ''
         }
-        this.getArticleDate = (timesData)  => {
-            const dateBegin = new Date(timesData)
-            const dateEnd = new Date()
-            const dateDiff = dateEnd.getTime() - dateBegin.getTime() // 时间差的毫秒数
-            const dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000)) // 计算出相差天数
-            const leave1 = dateDiff % (24 * 3600 * 1000)    // 计算天数后剩余的毫秒数
-            const hours = Math.floor(leave1 / (3600 * 1000))// 计算出小时数
-            //计算相差分钟数
-            const leave2 = leave1 % (3600 * 1000)    // 计算小时数后剩余的毫秒数
-            const minutes = Math.floor(leave2 / (60 * 1000))// 计算相差分钟数
-            //计算相差秒数
-            const leave3 = leave2 % (60 * 1000)      // 计算分钟数后剩余的毫秒数
-            const seconds = Math.round(leave3 / 1000)
-            let timesString = ''
-        
-            if (dayDiff != 0) {
-                timesString = dayDiff + '天之前'
-            } else if (dayDiff == 0 && hours != 0) {
-                timesString = hours + '小时之前'
-            } else if (dayDiff == 0 && hours == 0) {
-                timesString = minutes + '分钟之前'
-            }
-        
-            return timesString
-        }
-        // <ul className="article-nav">
-                    //     {
-                    //         navData.map(v => {
-                    //             return (
-                    //                 <li key={v.id}>{v.tag}</li>
-                    //             )
-                    //         })
-                    //     }
-                    // </ul>
     }
     componentWillMount() {
         this.getNav()
@@ -90,7 +57,7 @@ class Article extends Component {
             <div className="home-article-box">
                 <div className="home-article">
                     <div className="article-header_banner">
-                        <div className="header-banner_left">
+                        <div className={`header-banner_left ${!(list.list && list.list.length) ? 'zhanwei zhanwei-article' : '' }`}>
                             <Carousel >
                                 {
                                     list.major.map(v => {
@@ -105,6 +72,14 @@ class Article extends Component {
                             </Carousel>
                         </div>
                         <div className="header-banner_right">
+                            {
+                                !(list && list.list.length) && <ul>
+                                    <li className="zhanwei zhanwei-article"></li>
+                                    <li className="zhanwei zhanwei-article"></li>
+                                    <li className="zhanwei zhanwei-article"></li>
+                                    <li className="zhanwei zhanwei-article"></li>
+                                </ul>
+                            }
                             <ul>
                                 {
                                     list.major2.map(v => {
@@ -124,27 +99,29 @@ class Article extends Component {
                         <div className="article-center_title">
                             <h3>最新</h3>
                         </div>
-                        <ul className="article-center_text">
-                            {
-                                list.list.map(v => {
-                                    return (
-                                        <li className="center-text_li" key={v.id}>
-                                            <div className="text-li_left">
-                                                <img src={v.articleImg} />
-                                                <span className="article-tip_span">{v.title}</span>
-                                            </div>
-                                            <div className="text-li_text">
-                                                <span className="text-li_label text-li_dian label-tag">{this.getTagName(v.tagId)}</span>
-                                                <span className="text-li_label text-li_dian label-userName">{v.userName || '佚名'}</span>
-                                                <span className="text-li_label label-date">{this.getArticleDate(v.createDate)}</span>
-                                                <h4 className="text-li_header">{v.title}</h4>
-                                                <p className="text-li_center">{v.introduce}</p>
-                                            </div>
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
+                        <Skeleton active loading={!(list.list && list.list.length)}>
+                            <ul className="article-center_text">
+                                {
+                                    list.list.map(v => {
+                                        return (
+                                            <li className="center-text_li" key={v.id}>
+                                                <div className="text-li_left">
+                                                    <img src={v.articleImg} />
+                                                    <span className="article-tip_span">{v.title}</span>
+                                                </div>
+                                                <div className="text-li_text">
+                                                    <span className="text-li_label text-li_dian label-tag">{this.getTagName(v.tagId)}</span>
+                                                    <span className="text-li_label text-li_dian label-userName">{v.userName || '佚名'}</span>
+                                                    <span className="text-li_label label-date">{getArticleDate(v.createDate)}</span>
+                                                    <h4 className="text-li_header">{v.title}</h4>
+                                                    <p className="text-li_center">{v.introduce}</p>
+                                                </div>
+                                            </li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </Skeleton>
                     </div>
                 </div>
                 <div className="article-edit-box">
