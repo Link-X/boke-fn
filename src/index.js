013764e0fe8499ca8c4b36a958a0ca2cde7864e1
许道斌 <xudb01@mingyuanyun.com>
2019-11-13 10:34:03 +0800

    -

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import "babel-polyfill";
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Root from './router/Routes.js'
 import 'antd/lib/date-picker/style';
 import '@/common/less/reset.less'
 import { Form, Icon, Input, Button, Checkbox  } from 'antd'
-require('babel-polyfill')
 
 render( 
     <Root />,
