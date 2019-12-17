54f292d53c9cca925fc59c06a37e69b71a0077d1
许道斌 <xudb01@mingyuanyun.com>
2019-12-17 17:58:15 +0800

    update: 添加新增文章提示

--- src/view/edit-article/index.js
+++ src/view/edit-article/index.js
@@ -7,13 +7,66 @@ import { getTags, addArticle, uploadImage, getArticleDetails, editArticleDetials
 import '@/common/less/edit.less'
 import 'github-markdown-css'
 
-
 class EditArticle extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       form: {
-        markdown: '',
+        markdown: `
+#  H1
+##  H2
+###  H3
+####  H4
+#####  H5
+######  H6
+
+*斜体* or _强调_  
+**加粗** or __加粗__  
+***粗斜体*** or ___粗斜体__  
+
+Unordered 无序列表：
+* 无序列表
+* 子项
+* 子项
+ 
++ 无序列表
++ 子项
++ 子项
+ 
+- 无序列表
+- 子项
+- 子项
+
+Ordered 有序列表：
+* 产品介绍（子项无项目符号）
+    此时子项，要以一个制表符或者4个空格缩进
+ 
+* 产品特点
+    1. 特点1
+    - 特点2
+    - 特点3
+* 产品功能
+    1. 功能1
+    - 功能2
+    - 功能3
+    
+[链接](http://www.w3cschool.cn/ "W3Cschool")
+![图片代替文字](http://statics.w3cschool.cn/images/w3c/index-logo.png "标题文字")
+
+段落
+> Email-style angle brackets
+> are used for blockquotes.
+> > And, they can be nested.
+> #### Headers in blockquotes
+> * You can quote a list.
+> * Etc.
+
+表格  
+
+|姓名|性别|年龄|
+|:---:|:---:|:---:|:---:|
+| 许道斌 | 男 | 20岁 |
+`,
         tagId: '',
         title: '',
         articleImg: ''
@@ -70,6 +123,7 @@ class EditArticle extends React.Component {
     })
   }
   submit(e) {
+    console.log(this.state.form.markdown)
     e.stopPropagation()
     const msg = this.videtd()
     if (!msg.ok) {
@@ -272,7 +326,7 @@ class EditArticle extends React.Component {
             <input 
               value={form.title}
               onChange={this.setInp}
-              placeholder="请输入标题...">
+              placeholder="请输入文章标题...">
             </input>
           </div>
           <div className="edit-article-btn">
