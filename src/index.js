405f88143964c6b20e898063442d8cb55dc05386
许道斌 <xudb01@mingyuanyun.com>
2019-12-27 10:28:55 +0800

    update:　加入前端性能监控＇

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import { render } from 'react-dom';
 import Root from './router/Routes.js'
 import 'antd/lib/date-picker/style';
 import '@/common/less/reset.less'
+import wpkReporter from 'wpk-reporter' // 导入基础sdk
 
+const __wpk = new wpkReporter({
+  bid: 'xudaobin-dtagam3v', // 新建应用时确定
+  spa: true,  // 单页应用开启后，可更准确地采集PV
+  plugins: []
+})
+__wpk.installAll() // 初始化sdk 必须调用\
+console.log(__wpk);
 render( 
     <Root />,
     document.getElementById('app')
